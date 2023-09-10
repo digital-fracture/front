4d021abca27983f4f1a35e9e7d0d7bea2f0d1158
scarlettnik <87085957+scarlettnik@users.noreply.github.com>
2023-09-10 09:15:58 +0300

    Update GoogleSignInButton.tsx
    c

--- src/components/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton.tsx
@@ -7,7 +7,7 @@ interface GoogleSignInButtonProps {
 }
 const GoogleSignInButton: FC<GoogleSignInButtonProps> = ({ children }) => {
   const loginWithGoogle = () => signIn('google', {
-    callbackUrl: "http://localhost:3000/admin"
+    callbackUrl: "https://front-7.vercel.app/admin"
   });
 
   return (
